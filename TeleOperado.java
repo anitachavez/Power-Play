92c86f7802a79c0937f0b94032b5ab9c538b678a
anitachavez <114031655+anitachavez@users.noreply.github.com>
2022-10-10 20:23:50 -0500

    Update Rampa
    Adelanto de progra para rampa de motores, aún no está terminado.

--- TeleOperado.java
+++ TeleOperado.java
@@ -1,30 +1,39 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOp")
-public class TeleOperado extends OpMode{
+public class TeleOperado extends LinearOpMode{
+    final double UPDATE_PERIOD = 1000;
     DcMotor frontLeft, frontRight, backLeft, backRight;
-
-    // todo: write your code here
     
-     @Override
-        public void init(){
-            frontLeft = hardwareMap.get(DcMotor.class, "front_left");
-            frontRight = hardwareMap.get(DcMotor.class, "front_right");
-            backLeft = hardwareMap.get(DcMotor.class, "back_left");
-            backRight = hardwareMap.get(DcMotor.class, "back_right");  
-            frontLeft.setDirection(DcMotor.Direction.REVERSE);
-            backLeft.setDirection(DcMotor.Direction.REVERSE);
-     }
     @Override
-    public void loop(){
-        moveChasis(gamepad1);
-    }
+    public void runOpMode(){
+        frontLeft = hardwareMap.get(DcMotor.class, "front_left");
+        frontRight = hardwareMap.get(DcMotor.class, "front_right");
+        backLeft = hardwareMap.get(DcMotor.class, "back_left");
+        backRight = hardwareMap.get(DcMotor.class, "back_right");  
+        frontLeft.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.REVERSE);
+        ElapsedTime timer = new ElapsedTime();
+        double lastUpdateTime = timer.milliseconds();
+        waitForStart();
+        while(opModeIsActive()) {
+            double currentTime = timer.milliseconds();
+            if (currentTime - lastUpdateTime > UPDATE_PERIOD){
+                moveChasis(gamepad1);
+                telemetry.addData("actualizar motor", "");
+                lastUpdateTime = currentTime;
+            }
+            telemetry.update();
+        }
+     }
+     
         private void moveChasis(Gamepad gamepad) {
             double drive = -gamepad.left_stick_y;
             double lateral = gamepad.right_stick_x;
@@ -32,16 +41,17 @@ public class TeleOperado extends OpMode{
             double powerMultiplier = getPowerMultiplier(gamepad);
             move(drive, lateral, turn, powerMultiplier);
             
+            
         }
         
         private double getPowerMultiplier(Gamepad gamepad){
             if(gamepad.right_bumper && gamepad.left_bumper)
-                return 0.2;
-            if(gamepad.right_bumper)
-                return 0.6;
-            if(gamepad.left_bumper)
                 return 0.4;
-            return 0.8;
+            if(gamepad.right_bumper)
+                return 0.8;
+            if(gamepad.left_bumper)
+                return 0.6;
+            return 1.0;
         }
     
        /* private void controlCarousel(Gamepad gamepad){
@@ -61,14 +71,36 @@ public class TeleOperado extends OpMode{
             double frontRightPower = (drive - lateral - turn) * multiplier;
             double backLeftPower = (drive - lateral + turn) * multiplier;
             double backRightPower = (drive + lateral - turn) * multiplier;
+            
+            if (frontRight.getPower()<frontRightPower){
+            frontRightPower = frontRightPower + 0.1;
+            }
+            if (frontLeft.getPower()<frontLeftPower){
+            frontLeftPower = frontLeftPower + 0.1;
+            }
+            if(backRight.getPower()<backRightPower){
+            backRightPower = backRightPower + 0.1;
+            }
+            if (backLeft.getPower()<backLeftPower){
+            backLeftPower = backLeftPower + 0.1;
+            }
+            if (frontRight.getPower()>frontRightPower){
+            frontRightPower = frontRightPower - 0.1;
+            }
+            if (frontLeft.getPower()>frontLeftPower){
+            frontLeftPower = frontLeftPower - 0.1;
+            } 
+            if(backRight.getPower()>backRightPower){
+            backRightPower = backRightPower - 0.1;
+            } 
+            if (backLeft.getPower()>backLeftPower){
+            backLeftPower = backLeftPower - 0.1;
+            
+            }else
             frontLeft.setPower(Range.clip(frontLeftPower, -1, 1));
             frontRight.setPower(Range.clip(frontRightPower, -1, 1));
             backLeft.setPower(Range.clip(backLeftPower, -1, 1));
             backRight.setPower(Range.clip(backRightPower, -1, 1));
+            }
     }
     
-    
-    
-    
-    
-}
