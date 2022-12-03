ea01e9460f51547a2270d6868e37a268f10036e0
nautilus4010 <119711137+nautilus4010@users.noreply.github.com>
2022-12-02 18:25:56 -0600

    Update Basic: Omni Linear OpMode.java

--- Basic: Omni Linear OpMode.java
+++ Basic: Omni Linear OpMode.java
@@ -1,8 +1,7 @@
-
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.Range;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -10,8 +9,10 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Basic: Omni Linear OpMode", group="Linear Opmode")
 public class BasicOmniOpMode_Linear extends LinearOpMode {
+    final double UPDATE_PERIOD_MS = 50;
+    final double MOTOR_POWER_INCREMENT = 1;
 
-    private ElapsedTime runtime = new ElapsedTime();
+    private ElapsedTime timer = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
     private DcMotor rightFrontDrive = null;
@@ -35,39 +36,55 @@ public class BasicOmniOpMode_Linear extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
-        runtime.reset();
+        timer.reset();
+        double lastUpdateTime = timer.milliseconds();
 
         while (opModeIsActive()) {
-            double max;
-
+            double currentTime = timer.milliseconds();
             double axial   = -gamepad1.left_stick_y;
             double lateral =  gamepad1.left_stick_x;
             double yaw     = -gamepad1.right_stick_x;
 
-            double leftFrontPower  = axial - lateral + yaw;
-            double rightFrontPower = axial + lateral - yaw;
-            double leftBackPower   = axial + lateral + yaw;
-            double rightBackPower  = axial - lateral - yaw;
+            double leftFrontTarget  = axial - lateral + yaw;
+            double rightFrontTarget = axial + lateral - yaw;
+            double leftBackTarget   = axial + lateral + yaw;
+            double rightBackTarget  = axial - lateral - yaw;
 
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
+            double max = Math.max(Math.abs(leftFrontTarget), Math.abs(rightFrontTarget));
+            max = Math.max(max, Math.abs(leftBackTarget));
+            max = Math.max(max, Math.abs(rightBackTarget));
 
             if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+                leftFrontTarget  /= max;
+                rightFrontTarget /= max;
+                leftBackTarget   /= max;
+                rightBackTarget  /= max;
             }
-
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
-
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
-            telemetry.addData("Back  left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
+            double leftFrontPower = getIncreasedPower(leftFrontDrive.getPower(), leftFrontTarget);
+            double rightFrontPower = getIncreasedPower(rightFrontDrive.getPower(), rightFrontTarget);
+            double leftBackPower = getIncreasedPower(leftBackDrive.getPower(), leftBackTarget);
+            double rightBackPower = getIncreasedPower(rightBackDrive.getPower(), rightBackTarget);
+            if (currentTime - lastUpdateTime > UPDATE_PERIOD_MS){
+                leftFrontDrive.setPower(leftFrontTarget);
+                rightFrontDrive.setPower(rightFrontTarget);
+                leftBackDrive.setPower(leftBackTarget);
+                rightBackDrive.setPower(rightBackTarget);
+                lastUpdateTime = currentTime;
+            }
+            telemetry.addData("LF", leftFrontDrive.getPower() + " " + leftFrontPower);
+            telemetry.addData("RF", rightFrontDrive.getPower() + " " + rightFrontPower);
+            telemetry.addData("LB", leftBackDrive.getPower() + " " + leftBackPower);
+            telemetry.addData("RB", rightBackDrive.getPower() + " " + rightBackPower);
             telemetry.update();
         }
-    }}
+    }
+    
+    double getIncreasedPower(double current, double target) {
+        double diff = target - current;
+        double absDiff = Math.abs(diff);
+        double sign = diff / absDiff;
+        telemetry.addData("", sign);
+        double valueToIncrement = sign * Math.min(absDiff, MOTOR_POWER_INCREMENT);
+        return current + valueToIncrement;
+    }
+}
