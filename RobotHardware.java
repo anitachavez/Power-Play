a8e8698804bf23f64cbf962e7b13652d47305338
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2022-12-06 21:57:41 -0600

    Added RobotHardware class

---
+++ RobotHardware.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+public class RobotHardware {
+    public DcMotor leftFrontDrive;
+    public DcMotor leftBackDrive;
+    public DcMotor rightFrontDrive;
+    public DcMotor rightBackDrive;
+
+    private LinearOpMode opMode;
+
+    public RobotHardware(LinearOpMode opMode) {
+        this.opMode = opMode;
+    }
+
+    public void initializeHardware(HardwareMap hardwareMap) {
+        opMode.telemetry.addData("Status", "Initializing...");
+        opMode.telemetry.update();
+
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "front_left");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "back_left");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "front_right");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "back_right");
+
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        opMode.telemetry.addData("Status", "Initialized");
+        opMode.telemetry.update();
+    }
+}
--- Teleoperado.java
+++ Teleoperado.java
@@ -1,39 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.util.Range;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="Teleoperado", group="Linear Opmode")
+
+@TeleOp(name="Teleoperado")
 public class Teleoperado extends LinearOpMode {
     final double UPDATE_PERIOD_MS = 50;
     final double MOTOR_POWER_INCREMENT = 1;
 
+    final int LEFT_FRONT_POWER = 0;
+    final int RIGHT_FRONT_POWER = 1;
+    final int LEFT_BACK_POWER = 2;
+    final int RIGHT_BACK_POWER = 3;
+
     private ElapsedTime timer = new ElapsedTime();
-    private DcMotor leftFrontDrive = null;
-    private DcMotor leftBackDrive = null;
-    private DcMotor rightFrontDrive = null;
-    private DcMotor rightBackDrive = null;
+    private RobotHardware robot = new RobotHardware(this);
 
     @Override
     public void runOpMode() {
-
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "front_left");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "back_left");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "front_right");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "back_right");
-
-
-        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.update();
+        robot.initializeHardware(hardwareMap);
 
         waitForStart();
         timer.reset();
@@ -41,42 +28,49 @@ public class Teleoperado extends LinearOpMode {
 
         while (opModeIsActive()) {
             double currentTime = timer.milliseconds();
-            double axial   = -gamepad1.left_stick_y;
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     = gamepad1.right_stick_x;
-
-            double leftFrontTarget  = axial - lateral - yaw;
-            double rightFrontTarget = axial + lateral + yaw;
-            double leftBackTarget   = axial + lateral - yaw;
-            double rightBackTarget  = axial - lateral + yaw;
-
-            double max = Math.max(Math.abs(leftFrontTarget), Math.abs(rightFrontTarget));
-            max = Math.max(max, Math.abs(leftBackTarget));
-            max = Math.max(max, Math.abs(rightBackTarget));
-
-            if (max > 1.0) {
-                leftFrontTarget  /= max;
-                rightFrontTarget /= max;
-                leftBackTarget   /= max;
-                rightBackTarget  /= max;
-            }
-            double leftFrontPower = getIncreasedPower(leftFrontDrive.getPower(), leftFrontTarget);
-            double rightFrontPower = getIncreasedPower(rightFrontDrive.getPower(), rightFrontTarget);
-            double leftBackPower = getIncreasedPower(leftBackDrive.getPower(), leftBackTarget);
-            double rightBackPower = getIncreasedPower(rightBackDrive.getPower(), rightBackTarget);
             if (currentTime - lastUpdateTime > UPDATE_PERIOD_MS){
-                leftFrontDrive.setPower(leftFrontTarget);
-                rightFrontDrive.setPower(rightFrontTarget);
-                leftBackDrive.setPower(leftBackTarget);
-                rightBackDrive.setPower(rightBackTarget);
+                double[] chasisPowers = getChasisPowers();
+                robot.leftFrontDrive.setPower(chasisPowers[LEFT_FRONT_POWER]);
+                robot.rightFrontDrive.setPower(chasisPowers[RIGHT_FRONT_POWER]);
+                robot.leftBackDrive.setPower(chasisPowers[LEFT_BACK_POWER]);
+                robot.rightBackDrive.setPower(chasisPowers[RIGHT_BACK_POWER]);
                 lastUpdateTime = currentTime;
             }
-            telemetry.addData("LF", leftFrontDrive.getPower() + " " + leftFrontPower);
-            telemetry.addData("RF", rightFrontDrive.getPower() + " " + rightFrontPower);
-            telemetry.addData("LB", leftBackDrive.getPower() + " " + leftBackPower);
-            telemetry.addData("RB", rightBackDrive.getPower() + " " + rightBackPower);
+            
             telemetry.update();
         }
+    }
+
+    double[] getChasisPowers() {
+        double axial   = -gamepad1.left_stick_y;
+        double lateral =  gamepad1.left_stick_x;
+        double yaw     = gamepad1.right_stick_x;
+
+        double leftFrontTarget  = axial - lateral - yaw;
+        double rightFrontTarget = axial + lateral + yaw;
+        double leftBackTarget   = axial + lateral - yaw;
+        double rightBackTarget  = axial - lateral + yaw;
+
+        double max = Math.max(Math.abs(leftFrontTarget), Math.abs(rightFrontTarget));
+        max = Math.max(max, Math.abs(leftBackTarget));
+        max = Math.max(max, Math.abs(rightBackTarget));
+
+        if (max > 1.0) {
+            leftFrontTarget  /= max;
+            rightFrontTarget /= max;
+            leftBackTarget   /= max;
+            rightBackTarget  /= max;
+        }
+        double[] chasisPowers = new double[4];
+        chasisPowers[LEFT_FRONT_POWER] = getIncreasedPower(robot.leftFrontDrive.getPower(), leftFrontTarget);
+        chasisPowers[RIGHT_FRONT_POWER] = getIncreasedPower(robot.rightFrontDrive.getPower(), rightFrontTarget);
+        chasisPowers[LEFT_BACK_POWER] = getIncreasedPower(robot.leftBackDrive.getPower(), leftBackTarget);
+        chasisPowers[RIGHT_BACK_POWER] = getIncreasedPower(robot.rightBackDrive.getPower(), rightBackTarget);
+        telemetry.addData("LF", robot.leftFrontDrive.getPower() + " " + chasisPowers[LEFT_FRONT_POWER]);
+        telemetry.addData("RF", robot.rightFrontDrive.getPower() + " " + chasisPowers[RIGHT_FRONT_POWER]);
+        telemetry.addData("LB", robot.leftBackDrive.getPower() + " " + chasisPowers[LEFT_BACK_POWER]);
+        telemetry.addData("RB", robot.rightBackDrive.getPower() + " " + chasisPowers[RIGHT_BACK_POWER]);
+        return chasisPowers;
     }
     
     double getIncreasedPower(double current, double target) {
