237231139533a3073665a9df1c9ec8fc496ad4b4
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2023-01-15 20:37:37 -0600

    Move chasis logic to RobotHardware

--- RobotHardware.java
+++ RobotHardware.java
@@ -1,9 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RobotHardware {
     public DcMotor leftFrontDrive;
@@ -11,6 +11,11 @@ public class RobotHardware {
     public DcMotor rightFrontDrive;
     public DcMotor rightBackDrive;
 
+    final double MOTOR_UPDATE_PERIOD_MS = 50;
+    final double MOTOR_POWER_INCREMENT = 0.065;
+
+    private ElapsedTime timer = new ElapsedTime();
+    private double lastMotorUpdateTime;
     private LinearOpMode opMode;
 
     public RobotHardware(LinearOpMode opMode) {
@@ -31,8 +36,55 @@ public class RobotHardware {
         rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.REVERSE);
 
+        lastMotorUpdateTime = timer.milliseconds();
+
         opMode.telemetry.addData("Status", "Initialized");
         opMode.telemetry.update();
+    }
+
+    public void move(double drive, double lateral, double yaw) {
+        double currentTime = timer.milliseconds();
+        if (currentTime - lastMotorUpdateTime > MOTOR_UPDATE_PERIOD_MS) {
+            double leftFrontTarget  = drive - lateral - yaw;
+            double rightFrontTarget = drive + lateral + yaw;
+            double leftBackTarget   = drive + lateral - yaw;
+            double rightBackTarget  = drive - lateral + yaw;
+            // Normalise velocities
+            double max = Math.max(Math.abs(leftFrontTarget), Math.abs(rightFrontTarget));
+            max = Math.max(max, Math.abs(leftBackTarget));
+            max = Math.max(max, Math.abs(rightBackTarget));
+            if (max > 1.0) {
+                leftFrontTarget  /= max;
+                rightFrontTarget /= max;
+                leftBackTarget   /= max;
+                rightBackTarget  /= max;
+            }
+            // Ramp velocities
+            double leftFrontPower  = getIncreasedPower(leftFrontDrive.getPower(), leftFrontTarget);
+            double rightFrontPower = getIncreasedPower(rightFrontDrive.getPower(), rightFrontTarget);
+            double leftBackPower   = getIncreasedPower(leftBackDrive.getPower(), leftBackTarget);
+            double rightBackPower  = getIncreasedPower(rightBackDrive.getPower(), rightBackTarget);
+            // Update velocities
+            leftFrontDrive.setPower(leftFrontPower);
+            rightFrontDrive.setPower(rightFrontPower);
+            leftBackDrive.setPower(leftBackPower);
+            rightBackDrive.setPower(rightBackPower);
+            opMode.telemetry.addData("LF", leftFrontTarget + " " + leftFrontPower);
+            opMode.telemetry.addData("RF", rightFrontTarget + " " + rightFrontPower);
+            opMode.telemetry.addData("LB", leftBackTarget + " " + leftBackPower);
+            opMode.telemetry.addData("RB", rightBackTarget + " " + rightBackPower);
+            lastMotorUpdateTime = currentTime;
+        }
+    }
+
+    private double getIncreasedPower(double current, double target) {
+        double diff = target - current;
+        if(diff == 0)
+            return current;
+        double absDiff = Math.abs(diff);
+        double sign = diff / absDiff;
+        double valueToIncrement = sign * Math.min(absDiff, MOTOR_POWER_INCREMENT);
+        return current + valueToIncrement;
     }
 
     public void logChasisPowers() {
--- Teleoperado.java
+++ Teleoperado.java
@@ -2,20 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
 
 @TeleOp(name="Teleoperado")
 public class Teleoperado extends LinearOpMode {
-    final double UPDATE_PERIOD_MS = 50;
-    final double MOTOR_POWER_INCREMENT = 0.065;
-
-    final int LEFT_FRONT_POWER = 0;
-    final int RIGHT_FRONT_POWER = 1;
-    final int LEFT_BACK_POWER = 2;
-    final int RIGHT_BACK_POWER = 3;
-
-    private ElapsedTime timer = new ElapsedTime();
     private RobotHardware robot = new RobotHardware(this);
 
     @Override
@@ -23,63 +12,12 @@ public class Teleoperado extends LinearOpMode {
         robot.initializeHardware(hardwareMap);
 
         waitForStart();
-        timer.reset();
-        double lastUpdateTime = timer.milliseconds();
-
         while (opModeIsActive()) {
-            double currentTime = timer.milliseconds();
-            if (currentTime - lastUpdateTime > UPDATE_PERIOD_MS){
-                double[] chasisPowers = getChasisPowers();
-                robot.leftFrontDrive.setPower(chasisPowers[LEFT_FRONT_POWER]);
-                robot.rightFrontDrive.setPower(chasisPowers[RIGHT_FRONT_POWER]);
-                robot.leftBackDrive.setPower(chasisPowers[LEFT_BACK_POWER]);
-                robot.rightBackDrive.setPower(chasisPowers[RIGHT_BACK_POWER]);
-                lastUpdateTime = currentTime;
-            }
-            
+            double axial   = -gamepad1.left_stick_y;
+            double lateral =  gamepad1.left_stick_x;
+            double yaw     =  gamepad1.right_stick_x;
+            robot.move(axial, lateral, yaw);
             telemetry.update();
         }
-    }
-
-    double[] getChasisPowers() {
-        double axial   = -gamepad1.left_stick_y;
-        double lateral =  gamepad1.left_stick_x;
-        double yaw     = gamepad1.right_stick_x;
-
-        double leftFrontTarget  = axial - lateral - yaw;
-        double rightFrontTarget = axial + lateral + yaw;
-        double leftBackTarget   = axial + lateral - yaw;
-        double rightBackTarget  = axial - lateral + yaw;
-
-        double max = Math.max(Math.abs(leftFrontTarget), Math.abs(rightFrontTarget));
-        max = Math.max(max, Math.abs(leftBackTarget));
-        max = Math.max(max, Math.abs(rightBackTarget));
-
-        if (max > 1.0) {
-            leftFrontTarget  /= max;
-            rightFrontTarget /= max;
-            leftBackTarget   /= max;
-            rightBackTarget  /= max;
-        }
-        double[] chasisPowers = new double[4];
-        chasisPowers[LEFT_FRONT_POWER] = getIncreasedPower(robot.leftFrontDrive.getPower(), leftFrontTarget);
-        chasisPowers[RIGHT_FRONT_POWER] = getIncreasedPower(robot.rightFrontDrive.getPower(), rightFrontTarget);
-        chasisPowers[LEFT_BACK_POWER] = getIncreasedPower(robot.leftBackDrive.getPower(), leftBackTarget);
-        chasisPowers[RIGHT_BACK_POWER] = getIncreasedPower(robot.rightBackDrive.getPower(), rightBackTarget);
-        telemetry.addData("LF", leftFrontTarget + " " + chasisPowers[LEFT_FRONT_POWER]);
-        telemetry.addData("RF", rightFrontTarget + " " + chasisPowers[RIGHT_FRONT_POWER]);
-        telemetry.addData("LB", leftBackTarget + " " + chasisPowers[LEFT_BACK_POWER]);
-        telemetry.addData("RB", rightBackTarget + " " + chasisPowers[RIGHT_BACK_POWER]);
-        return chasisPowers;
-    }
-    
-    double getIncreasedPower(double current, double target) {
-        double diff = target - current;
-        if(diff == 0)
-            return current;
-        double absDiff = Math.abs(diff);
-        double sign = diff / absDiff;
-        double valueToIncrement = sign * Math.min(absDiff, MOTOR_POWER_INCREMENT);
-        return current + valueToIncrement;
     }
 }
